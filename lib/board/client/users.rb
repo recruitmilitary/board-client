c3271d0b6f5e3c04e68e79854b0bd4ca4b9e4dcf
Michael Guterl <michael@diminishing.org>
2011-08-26 13:19:40 -0400

    find users by email and email_md5

--- lib/board/client/users.rb
+++ lib/board/client/users.rb
@@ -10,7 +10,11 @@ module Board
     end
 
     def find(id)
-      Hashie::Mash.new(@client.get("/users/#{id}"))
+      if id.is_a?(Hash)
+        Hashie::Mash.new(@client.get("/users", id))
+      else
+        Hashie::Mash.new(@client.get("/users/#{id}"))
+      end
     end
 
   end
--- spec/integration/users/find_spec.rb
+++ spec/integration/users/find_spec.rb
@@ -6,7 +6,7 @@ describe 'User Find' do
 
   context 'by id' do
     context 'when the user exists' do
-      use_vcr_cassette "user exists with id"
+      use_vcr_cassette 'user exists with id'
 
       it 'returns a user with the correct attributes' do
         user = board.users.find(100)
@@ -19,7 +19,7 @@ describe 'User Find' do
     end
 
     context 'when the user does not exist' do
-      use_vcr_cassette "user id does not exist"
+      use_vcr_cassette 'user id does not exist'
 
       it 'raises a not found exception' do
         expect {
@@ -30,12 +30,46 @@ describe 'User Find' do
   end
 
   context 'by email' do
-    context 'when the user exists'
-    context 'when the user does not exist'
+    context 'when the user exists' do
+      use_vcr_cassette 'user exists with email'
+
+      it 'returns a user with the correct attributes' do
+        user = board.users.find(:email => 'candidate@recruitmilitary.com')
+
+        user.email.should == 'candidate@recruitmilitary.com'
+      end
+    end
+
+    context 'when the user does not exist' do
+      use_vcr_cassette 'user email does not exist'
+
+      it 'raises a not found exception' do
+        expect {
+          board.users.find(:email => 'michael@jordan.com')
+        }.to raise_error(Board::Request::NotFound)
+      end
+    end
   end
 
   context 'by email_md5' do
-    context 'when the user exists'
-    context 'when the user does not exist'
+    context 'when the user exists' do
+      use_vcr_cassette 'user exists with email md5'
+
+      it 'returns a user with the correct attributes' do
+        user = board.users.find(:email_md5 => '7618e7a4937b37640c24149bdf21cf27')
+
+        user.email.should == 'candidate@recruitmilitary.com'
+      end
+    end
+
+    context 'when the user does not exist' do
+      use_vcr_cassette 'user email md5 does not exist'
+
+      it 'raises a not found exception' do
+        expect {
+          board.users.find(:email_md5 => '93e8d736de5088cb392465e024b69eef')
+        }.to raise_error(Board::Request::NotFound)
+      end
+    end
   end
 end
