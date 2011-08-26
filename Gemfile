e8f89f1aa7ebb613cc8c2fb4d3883af8626ba01e
Michael Guterl <michael@diminishing.org>
2011-08-26 10:16:30 -0400

    upgrade to RSpec2 and latest WebMock

--- Gemfile
+++ Gemfile
@@ -4,6 +4,6 @@ gem "rest-client", "~> 1.6.1"
 gem "yajl-ruby", "~> 0.7.7"
 
 group :test do
-  gem "rspec", "~> 1.3.0"
-  gem "webmock", "~> 1.3.5"
+  gem "rspec", "~> 2.6.0"
+  gem "webmock", "~> 1.7.4"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'board-client'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
 require 'webmock/rspec'
-
 require 'yajl/json_gem' # required for webmock
 
-Spec::Runner.configure do |config|
-  config.include WebMock
+RSpec.configure do |config|
+  config.include WebMock::API
 end
